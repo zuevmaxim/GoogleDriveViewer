61de63494b590bd51ab0d1f4090058f2b640a5c3
maria <tigmah@mail.ru>
2020-05-20 18:47:46 +0300

    Add paging

--- src/main/kotlin/example/GoogleDriveClient.kt
+++ src/main/kotlin/example/GoogleDriveClient.kt
@@ -6,14 +6,23 @@ import org.json.JSONObject
 class GoogleDriveClient(private val apiKey : String, private val accessToken : String) {
 
     fun getAllFiles() : List<String> {
-        val response : Response = khttp.get(
+        var response : Response = khttp.get(
             "https://www.googleapis.com/drive/v2/files?",
             params = mapOf("key" to apiKey),
             headers = mapOf("Authorization" to "Bearer $accessToken", "Accept" to "application/json")
         )
-        val obj : JSONObject = response.jsonObject
-        return obj.getJSONArray("items")
-            .toList()
-            .map { item -> (item as JSONObject)["title"].toString() }
+        var files = mutableListOf<String>()
+        while (true) {
+            val obj : JSONObject = response.jsonObject
+            files.addAll(obj.getJSONArray("items").toList().map { item -> JSONObject(item)["title"].toString() })
+            if (obj["incompleteSearch"] as Boolean) {
+                return files;
+            }
+            response = khttp.get(
+                    obj["nextLink"].toString(),
+                    params = mapOf("key" to apiKey),
+                    headers = mapOf("Authorization" to "Bearer $accessToken", "Accept" to "application/json")
+            )
+        }
     }
 }
