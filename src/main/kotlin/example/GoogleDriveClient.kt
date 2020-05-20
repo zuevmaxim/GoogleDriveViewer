0affb69842fd0a72b80691cf0a30935332e05b04
maria <tigmah@mail.ru>
2020-05-20 17:43:00 +0300

    Add json parsing

--- src/main/kotlin/example/GoogleDriveClient.kt
+++ src/main/kotlin/example/GoogleDriveClient.kt
@@ -5,13 +5,15 @@ import org.json.JSONObject
 
 class GoogleDriveClient(private val apiKey : String, private val accessToken : String) {
 
-    public fun getAllFiles() : String? {
+    fun getAllFiles() : List<String> {
         val response : Response = khttp.get(
             "https://www.googleapis.com/drive/v2/files?",
             params = mapOf("key" to apiKey),
             headers = mapOf("Authorization" to "Bearer $accessToken", "Accept" to "application/json")
         )
         val obj : JSONObject = response.jsonObject
-        return obj.toString()
+        return obj.getJSONArray("items")
+            .toList()
+            .map { item -> JSONObject(item)["title"].toString() }
     }
 }
